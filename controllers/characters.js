76430aa124f2fc5a9f8d5e320ed5bf1b50e62675
Dawson Myers <Djmyers2002@gmail.com>
2023-04-24 13:57:15 -0400

    Adds Index View Of All Characters Functionality

--- controllers/characters.js
+++ controllers/characters.js
@@ -2,8 +2,16 @@ import { name } from "ejs";
 import { Character } from "../models/character.js";
 
 function index(req, res){
+  Character.find({})
+  .then(characters => {
   res.render('characters/index', {
+    characters: characters,
     title: 'All Characters'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/characters')
   })
 }
 
